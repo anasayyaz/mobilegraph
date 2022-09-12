da7d498fbad3700c43658e290fcb95b7d4046d69
Anas Ayyaz <anas.ayyazz@gmail.com>
2022-09-12 16:50:56 +0500

    commit

--- src/TempGraph.js
+++ src/TempGraph.js
@@ -86,7 +86,7 @@ console.log(graphData);
       alignItems: 'center',
       justifyContent: 'center',
       height: '100vh',
-      width:' 100xw'
+      width:' 100vw'
     }}>
          
       {options && (
